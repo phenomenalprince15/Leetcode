d4706df4bfd328ba356e2c99ecc30b08d7753c8f
Prince <phenomenalprince15@gmail.com>
2024-09-13 00:37:08 +0530

    dp

--- 19_Dynamic_Programming/Atcoder/README.md
+++ 19_Dynamic_Programming/Atcoder/README.md
@@ -1,1 +1,9 @@
 # Atcoder
+
+## Problems
+
+
+6. LCS :: https://atcoder.jp/contests/dp/tasks/dp_f
+
+8. Grid 1 :: https://atcoder.jp/contests/dp/tasks/dp_h
+
---
+++ 19_Dynamic_Programming/CSES/00_Dice_Combinations.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+
+const long long MOD = 1000000007;
+
+#define ll long long
+
+using namespace std;
+
+/*
+Problem ::
+Your task is to count the number of ways to construct sum n by throwing a dice one or more times. Each throw produces an outcome between 1 and  6.
+For example, if n=3, there are 4 ways:
+
+1+1+1
+1+2
+2+1
+3
+
+Input
+The only input line has an integer n.
+Output
+Print the number of ways modulo 10^9+7.
+Constraints
+
+1 \le n \le 10^6
+
+Example
+Input:
+3
+
+Output:
+4
+*/
+
+
+
